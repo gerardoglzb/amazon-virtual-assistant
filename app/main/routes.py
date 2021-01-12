67292bc16692f8fca485ebc266896df11261be98
Gerardo González Becerril <gerbecdev@gmail.com>
2021-01-11 23:19:19 -0600

    Error handling in product operations.

--- app/main/routes.py
+++ app/main/routes.py
@@ -22,13 +22,3 @@ def home():
 @main.route('/about')
 def about():
 	return render_template('about.html', title="About", active=1)
-
-
-@main.route('/test')
-def test():
-	if current_user.is_authenticated:
-		page = request.args.get('page', 1, type=int)
-		user = User.query.filter_by(username=current_user.username).first_or_404()
-		products = Product.query.filter_by(author=user).order_by(Product.id.desc()).paginate(page=page, per_page=5)
-		return render_template('index.html', products=products)
-	return render_template('layout_test.html', title="Layout Test", active=1)
