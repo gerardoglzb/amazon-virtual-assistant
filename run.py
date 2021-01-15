6360c2e60b3ce9fab54b22ad0ccffe6159cd0b5d
Gerardo González Becerril <gerbecdev@gmail.com>
2021-01-14 18:44:49 -0600

    Environment variables setup for Heroku.

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app import create_app
 from app import db
+import os
 
 app = create_app()
 with app.app_context():
 	db.create_all()
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=os.environ.get('DEBUG_VALUE')=="True")
